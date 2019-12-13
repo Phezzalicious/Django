0599dd3ad08cbdaf7960f0bf5dfc793fd03d3215
Phezzalicious <starfish206@gmail.com>
2019-12-13 03:04:17 -0600

    break

--- RestChess/statsboard/views.py
+++ RestChess/statsboard/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the StatsBoard index.")
+def detail(request,user_name):
+    return HttpResponse("You're looking at Player %s." % user_name)
+
+def results(request, user_name):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % user_name)
+
+def vote(request, user_name):
+    return HttpResponse("You're voting on question %s." % user_name)
